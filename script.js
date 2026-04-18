4b9e7ede2a263b9864c4980b21512624f0dce3ff
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 23:37:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Portfolio Loaded - Stable Mode");
+const sections = document.querySelectorAll(".section");
+
+const reveal = () => {
+sections.forEach(sec => {
+const top = sec.getBoundingClientRect().top;
+
+if(top < window.innerHeight - 100){
+sec.classList.add("show");
+}
+});
+};
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+console.log("Cinematic Pro Portfolio Loaded");
