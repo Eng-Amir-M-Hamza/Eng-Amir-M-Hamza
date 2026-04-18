f805c6dffa6c21aabe941d6371a08e9030267906
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 22:50:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-function reveal() {
-  const cards = document.querySelectorAll(".card");
-
-  for (let i = 0; i < cards.length; i++) {
-    const windowHeight = window.innerHeight;
-    const elementTop = cards[i].getBoundingClientRect().top;
-
-    if (elementTop < windowHeight - 100) {
-      cards[i].style.opacity = 1;
-      cards[i].style.transform = "translateY(0)";
-      cards[i].style.transition = "0.8s ease";
-    }
-  }
-}
-
-window.addEventListener("scroll", reveal);
-reveal();
+console.log("Portfolio Loaded Successfully");
