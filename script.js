dbe33772af24a234d5f1945ee50d4d4b2efe261e
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 23:57:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const sections = document.querySelectorAll(".section");
-
-const reveal = () => {
-sections.forEach(sec => {
-const top = sec.getBoundingClientRect().top;
-
-if(top < window.innerHeight - 100){
-sec.classList.add("show");
-}
-});
-};
-
-window.addEventListener("scroll", reveal);
-reveal();
-
-console.log("Cinematic Pro Portfolio Loaded");
+console.log("Recruiter Portfolio Loaded");
