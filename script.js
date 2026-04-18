112e297292136ad5efd0c7b42a54f7bc323bf4b0
Eng-Amir-M-Hamza <amirautodeskacc4@gmail.com>
2026-04-18 22:48:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-function revealOnScroll() {
-const elements = document.querySelectorAll(".reveal, .card");
+function reveal() {
+  const cards = document.querySelectorAll(".card");
 
-for (let i = 0; i < elements.length; i++) {
-let windowHeight = window.innerHeight;
-let elementTop = elements[i].getBoundingClientRect().top;
-let elementVisible = 100;
+  for (let i = 0; i < cards.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = cards[i].getBoundingClientRect().top;
 
-if (elementTop < windowHeight - elementVisible) {
-elements[i].classList.add("active");
-}
-}
+    if (elementTop < windowHeight - 100) {
+      cards[i].style.opacity = 1;
+      cards[i].style.transform = "translateY(0)";
+      cards[i].style.transition = "0.8s ease";
+    }
+  }
 }
 
-window.addEventListener("scroll", revealOnScroll);
+window.addEventListener("scroll", reveal);
+reveal();
